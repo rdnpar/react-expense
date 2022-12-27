433bd5f65ff745b2d754759c172ba40124498e48
neilp <neil.parrilla@gmail.com>
2022-12-27 15:30:28 -0800

    Completed rendering lists and conditional content

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,19 +1,36 @@
 import "./NewExpense.css";
 import ExpenseForm from "./ExpenseForm";
+import { useState } from 'react';
 
 const NewExpense = (props) => {
+  const [isEditing, setIsEditing] = useState(false);
+
   const saveExpenseDataHandler = (enteredExpenseData) => {
     const expenseData = {
       ...enteredExpenseData,
       id: Math.random().toString(),
     };
-    console.log(expenseData);
     props.onAddExpense(expenseData);
+    setIsEditing(false);
   };
+
+  const startEditingHandler = () => {
+    setIsEditing(true);
+  }
+
+  const stopEditingHandler = () => {
+    setIsEditing(false);
+  }
+
+  let selectAddExpense = <button onClick={startEditingHandler}>Add New Expense</button>
+
+  if (isEditing === true) {
+    selectAddExpense = <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} onCancel={stopEditingHandler} />
+  }
 
   return (
     <div className="new-expense">
-      <ExpenseForm onSaveExpenseData = {saveExpenseDataHandler} />
+      {selectAddExpense}
     </div>
   );
 };
